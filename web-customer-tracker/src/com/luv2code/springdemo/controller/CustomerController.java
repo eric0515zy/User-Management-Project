2049f1d3af4ba9977abdfae7d67f7ef92c55d380
Eric Zhang <yz4801@nyu.edu>
2022-10-01 14:49:38 -0700

    User Management Project Code

---
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,67 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	// inject DAO
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {
+		// get customers from DAO
+		List<Customer> theCustomers = customerService.getCustomers();
+		// process customers
+		theModel.addAttribute("customers",theCustomers);
+		return "list-customers";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		customerService.saveCustomer(theCustomer);
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate (@RequestParam("customerId") int theId, Model theModel) {
+		
+		// get customer given id
+		Customer theCustomer = customerService.getCustomer(theId);
+		// set him as a model attribute
+		theModel.addAttribute("customer", theCustomer);
+		// send to form
+		return "customer-form";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId) {
+		// delete 
+		customerService.deleteCustomer(theId);
+		return "redirect:/customer/list";
+	}
+}
